4d89440feee3e15dd9102c0ea249270c321e6330
river2056 <chen0625tung@gmail.com>
2022-02-23 01:38:24 +0800

    + added self-implemented DoublyLinkedList.py + rename LinkList into LinkedList

---
+++ link_list/DoublyLinkedList.py
@@ -0,0 +1,128 @@
+class Node():
+    def __init__(self, value, prev, next):
+        self.value = value
+        self.prev = prev
+        self.next = next
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'[value: {self.value}, prev: {self.prev}, next: {self.next}]'
+
+class DoublyLinkedList():
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        """appends new values in the end of the list"""
+        node = Node(value, None, None)
+        if self.size <= 0:
+            self.head = node
+            self.tail = node
+        else:
+            original_tail = self.tail
+            node.prev = original_tail
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
+        return node
+
+    def prepend(self, value):
+        """appends new values in the beginning of the list"""
+        node = Node(value, None, None)
+        if self.size <= 0:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head.prev = node
+            self.head = node
+        self.size += 1
+        return node
+
+    def insert(self, index, value):
+        """insert new values in the middle of the list"""
+        node = Node(value, None, None)
+        previous_node = self.transverse_to_index(index - 1)
+        current_node = previous_node.next
+
+        '''
+            example:
+                prev<-->current
+                     ||
+                     \/
+            prev<-->node<-->current
+        '''
+        previous_node.next = node
+        current_node.prev = node
+        node.prev = previous_node
+        node.next = current_node
+        return node
+
+    def remove(self, index):
+        """remove values from list according to index"""
+        previous_node = self.transverse_to_index(index - 1)
+        to_delete = previous_node.next
+        next_node = to_delete.next
+
+        '''
+            example:
+            prev<-->to_delete<-->next
+                      ||
+                      \/
+                 prev<-->next
+        '''
+        previous_node.next = next_node
+        next_node.prev = previous_node
+        # reset pointers
+        to_delete.next = None
+        to_delete.prev = None
+        return to_delete
+
+    def transverse_to_index(self, index):
+        """
+            helper method to transverse through list 
+            and return node at given index
+        """
+        counter = 0
+        if index >= self.size - 1:
+            index = self.size - 1
+        if index <= 0:
+            index = 0
+        if self.size <= 0:
+            return None
+        
+        current_node = self.head
+        while counter != index:
+            current_node = current_node.next
+            counter += 1
+        return current_node
+
+    def print(self):
+        """iteratively prints all values in list"""
+        s = []
+        current = self.head
+        while current != None:
+            s.append(current.value)
+            current = current.next
+        print(s)
+
+def main():
+    my_doubly_linked_list = DoublyLinkedList()
+    my_doubly_linked_list.append(100)
+    my_doubly_linked_list.append(200)
+    my_doubly_linked_list.prepend(500)
+    my_doubly_linked_list.prepend(600)
+    inserted_node = my_doubly_linked_list.insert(2, 777)
+    print('inserted node: ', inserted_node.__repr__())
+    my_doubly_linked_list.print()
+
+    removed_node = my_doubly_linked_list.remove(2)
+    print('removed node: ', removed_node.__repr__())
+    my_doubly_linked_list.print()
+
+if __name__ == '__main__':
+    main()
---
+++ link_list/LinkedList.py
@@ -0,0 +1,130 @@
+class Node():
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'[{self.value}, {self.next}]'
+
+class LinkedList():
+    def __init__(self):
+        self.size = 0
+        self.head = None
+        self.tail = None
+
+    def append(self, value):
+        """appends new values to the end of the list"""
+        node = Node(value, None)
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            self.tail.next = node
+            self.tail = node
+        self.size += 1
+        return node
+
+    def prepend(self, value):
+        """appends new values in the beginning of the list"""
+        node = Node(value, None)
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            node.next = self.head
+            self.head = node
+        self.size += 1
+        return node
+
+    def insert(self, index, value):
+        """insert new values in the middle of the list"""
+        init_index = 0
+        node = Node(value, None)
+        if index == 0:
+            return self.prepend(value)
+        elif index >= self.size - 1:
+            return self.append(value)
+
+        if self.size == 0:
+            self.head = node
+            self.tail = node
+        else:
+            previous_node = self.head
+            current_node = self.head
+            while init_index != index:
+                previous_node = current_node
+                current_node = current_node.next
+                init_index += 1
+            previous_node.next = node
+            node.next = current_node
+        self.size += 1
+        return node
+
+    def remove(self, index):
+        """remove values from list according to index"""
+        if self.size <= 0:
+            return None
+        if index >= self.size - 1:
+            index = self.size - 1
+        if index <= 0:
+            original_head = self.head
+            self.head = self.head.next
+            return original_head
+        init_index = 0
+        previous_node = self.head
+        current_node = self.head
+        while init_index != index:
+            previous_node = current_node
+            current_node = current_node.next
+            init_index += 1
+        previous_node.next = current_node.next
+        self.size -= 1
+        current_node.next = None
+        return current_node
+
+    def print(self):
+        """iteratively prints all values in list"""
+        s = []
+        current = self.head
+        while current != None:
+            s.append(current.value)
+            current = current.next
+        print(s)
+
+# def print_link_list(llist):
+#     if llist == None:
+#         return
+#     print(llist)
+#     print_link_list(llist.next)
+    # current = llist.head
+    # while current != None:
+    #     print(current)
+    #     current = current.next
+    # return
+
+def main():
+    my_link_list = LinkedList()
+    my_link_list.append(100)
+    my_link_list.append(200)
+    my_link_list.prepend(500)
+    my_link_list.prepend(600)
+    my_link_list.insert(2, 999)
+    my_link_list.insert(5, 888)
+    my_link_list.print()
+    print(my_link_list.size)
+
+    removed_node = my_link_list.remove(2)
+    print(f'removed node: {removed_node.__repr__()}')
+    my_link_list.print()
+    print(my_link_list.size)
+
+    removed_node = my_link_list.remove(99)
+    print(f'removed node: {removed_node.__repr__()}')
+    my_link_list.print()
+    print(my_link_list.size)
+
+if __name__ == '__main__':
+    main()
