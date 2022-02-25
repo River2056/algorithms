0f345860ac5e5fa20776fc05d0c966fa08997ffb
kevin.tung <chen0625tung@gmail.com>
2022-02-25 13:30:26 +0800

    + reorganize Stack implementations into separate files + modified Stack method implementations

---
+++ data_structures/stacks/array_stack/Stack.py
@@ -0,0 +1,42 @@
+class Stack():
+    def __init__(self):
+        self.data = []
+
+    def peek(self):
+        return self.data[-1]
+
+    def push(self, value):
+        self.data.append(value)
+
+    def pop(self):
+        return self.data.pop(-1)
+
+    def size(self):
+        return len(self.data)
+
+    def print(self):
+        print(self.data)
+
+def main():
+    my_stack = Stack()
+    my_stack.push('google')
+    my_stack.push('udemy')
+    my_stack.push('discord')
+    my_stack.push('medium')
+    my_stack.push('youtube')
+    my_stack.print()
+
+    pop_item = my_stack.pop()
+    print('pop item: ', pop_item)
+    my_stack.print()
+
+    pop_item = my_stack.pop()
+    print('pop item: ', pop_item)
+    my_stack.print()
+
+    peek_item = my_stack.peek()
+    print('peek item: ', peek_item)
+    my_stack.print()
+
+if __name__ == '__main__':
+    main()
---
+++ data_structures/stacks/link_stack/Stack.py
@@ -0,0 +1,81 @@
+class Node():
+    def __init__(self, value, next):
+        self.value = value
+        self.next = next
+
+    def __str__(self):
+        return str(self.value)
+
+    def __repr__(self):
+        return f'[{self.value}, {self.next}]'
+
+
+class Stack():
+    def __init__(self):
+        self.top = None
+        self.bottom = None
+        self.length = 0
+
+    def peek(self):
+        print('peek top item: ', self.top)
+        return self.top
+
+    def push(self, value):
+        node = Node(value, None)
+        if self.length <= 0:
+            self.top = node
+            self.bottom = node
+        else:
+            original_top = self.top
+            self.top = node
+            self.top.next = original_top
+        self.length += 1
+        return node
+
+    def pop(self):
+        if self.length <= 0:
+            return None
+        pop_item = self.top
+        self.top = self.top.next
+        self.length -= 1
+
+        # reset pointer
+        pop_item.next = None
+        return pop_item
+
+    def is_empty(self):
+        return self.length == 0
+
+    def print(self):
+        s = []
+        current = self.top
+        while current != None:
+            s.append(current.value)
+            current = current.next
+        s.reverse()
+        print(s)
+
+
+def main():
+    my_stack = Stack()
+    my_stack.push('google')
+    my_stack.push('udemy')
+    my_stack.push('discord')
+    my_stack.push('medium')
+    my_stack.push('youtube')
+    my_stack.print()
+
+    pop_item = my_stack.pop()
+    print('pop item: ', pop_item)
+    my_stack.print()
+
+    pop_item = my_stack.pop()
+    print('pop item: ', pop_item)
+    my_stack.print()
+
+    peek_item = my_stack.peek()
+    print('peek item: ', peek_item)
+    my_stack.print()
+
+if __name__ == '__main__':
+    main()
