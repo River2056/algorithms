1c9fd7cbe2be6713b7c5a487fad84a798c15e3e3
river2056 <chen0625tung@gmail.com>
2022-03-16 01:23:54 +0800

    + added self implemented rotate matrix

---
+++ problems/rotate_matrix_self.py
@@ -0,0 +1,32 @@
+def rotate_matrix(m):
+    n = len(m)
+    for i in range(n):
+        for j in range(0, i):
+            t = m[j][i]
+            m[j][i] = m[i][j]
+            m[i][j] = t
+    return m
+
+def print_matrix(m):
+    for _, arr in enumerate(m):
+        for val in arr:
+            print(val, end='\t')
+        print()
+    print()
+
+def main():
+    m = [
+        [1, 2, 3, 4],
+        [5, 6, 7, 8],
+        [9, 10, 11, 12],
+        [13, 14, 15, 16],
+    ]
+    print('before')
+    print_matrix(m)
+
+    print('after')
+    rotated_matrix = rotate_matrix(m)
+    print_matrix(rotated_matrix)
+
+if __name__ == '__main__':
+    main()
