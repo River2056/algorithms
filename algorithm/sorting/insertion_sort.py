7d485afed4fdbcc196c2da605c2aecb39151a153
kevin.tung <chen0625tung@gmail.com>
2022-03-18 18:56:30 +0800

    + added insertion sort(completed) + modified common.py(still need to modify check array equals function)

--- algorithm/sorting/insertion_sort.py
+++ algorithm/sorting/insertion_sort.py
@@ -2,7 +2,11 @@ def insertion_sort(array):
     arr = [val for val in array]
     for i in range(len(arr)):
         if arr[i] < arr[0]:
-
+            arr.insert(0, arr.pop(i))
+        else:
+            for j in range(1, i):
+                if arr[i] >= arr[j-1] and arr[i] <= arr[j]:
+                    arr.insert(j, arr.pop(i))
     return arr
 
 def main():
@@ -12,7 +16,7 @@ def main():
     sys.path.insert(0, str(Path(os.path.abspath(__file__)).parent.parent.parent))
     from common import print_sort_result_benchmark
 
-    print_sort_result_benchmark(10, 1, 100, insertion_sort)
+    print_sort_result_benchmark(10, 1, 10, insertion_sort)
 
 if __name__ == '__main__':
     main()
--- common.py
+++ common.py
@@ -3,23 +3,38 @@ import random
 
 def print_sort_result_benchmark(number_of_elements, arr_start, arr_end, fn):
     arr = [random.randint(arr_start, arr_end) for i in range(number_of_elements)]
-
+    # arr = []
+    # while True:
+    #     num = random.randint(arr_start, arr_end)
+    #     while num not in arr and len(arr) < number_of_elements:
+    #         arr.append(num)
+    #         num = random.randint(arr_start, arr_end)
+    #     break
+    
     start_time = time.time()
     sorted_arr = fn(arr)
     end_time = time.time()
 
-    print('\n---------- results ----------\n')
+    check_if_equals = check_arr_content_equals(arr, sorted_arr)
+
+    print('---------- results ----------')
     print('before')
     print(arr)
-    print()
 
     print('after')
     print(sorted_arr)
-    print('\n---------- results ----------\n')
+    print(f'both arrays are equal: {check_if_equals}')
+    print('---------- results ----------')
 
-    print('\n---------- time ----------\n')
+    print('---------- time ----------')
 
     print('time elapsed: ')
     print(f'--- {end_time - start_time} seconds ---')
 
-    print('\n---------- time ----------\n')
+    print('---------- time ----------')
+
+def check_arr_content_equals(before, after):
+    for _, v in enumerate(after):
+        if not v in before:
+            return False
+    return True
