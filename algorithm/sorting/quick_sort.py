fea27324f6d356ca4377c81ad3f6ddf87ad513e1
Tung Chin Chen <chenp625tung@gmail.com>
2022-03-21 13:12:29 +0800

    added insertion sort
    + added insertion sort(not finished)

---
+++ algorithm/sorting/quick_sort.py
@@ -0,0 +1,31 @@
+def quick_sort(array):
+    if len(array) == 1:
+        return array
+    arr = [val for val in array]
+    pivot = arr[-1]
+    i, j = -1, 0
+    for e in range(len(arr)):
+        if e == len(arr) - 1:
+            i += 1
+            tmp = arr[i]
+            arr[i] = pivot
+            arr[e] = tmp
+            
+        if arr[e] < pivot:
+            i += 1
+            tmp = arr[i]
+            arr[i] = arr[j]
+            arr[j] = tmp
+        j += 1
+
+def main():
+    # import custom print function
+    import os, sys
+    from pathlib import Path
+    sys.path.insert(0, str(Path(os.path.abspath(__file__)).parent.parent.parent))
+    from common import print_sort_result_benchmark
+
+    print_sort_result_benchmark(10, 1, 100, quick_sort)
+
+if __name__ == '__main__':
+    main()
