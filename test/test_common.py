b77f5830822c9ac968701a0d1d457d2842bfe39f
Tung Chin Chen <chenp625tung@gmail.com>
2022-03-21 22:03:49 +0800

    assert add message
    + assert added error message

--- test/test_common.py
+++ test/test_common.py
@@ -6,7 +6,7 @@ def main():
 
     for i in range(500):
         arr = generate_random_int_array(10, 1, 100)
-        assert(len(arr) == 10)
+        assert(len(arr) == 10), 'generated array len not match parameter!'
 
 if __name__ == '__main__':
     main()
