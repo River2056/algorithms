4c16c015ecdeb5cd8b70a1aba3936e2777125097
river2056 <chen0625tung@gmail.com>
2022-04-14 00:21:40 +0800

    + added leetcode:valid_palindrome problem

---
+++ problems/valid_palindrome.py
@@ -0,0 +1,22 @@
+import re
+import os, sys
+from pathlib import Path
+sys.path.insert(0, str(Path(os.path.abspath(__file__)).parent.parent))
+from common import print_result
+
+def is_palindrome(s):
+    replaced = ''.join(re.findall(r'[a-zA-Z0-9]', s)).lower()
+    reverse = []
+    for i in range(len(replaced) - 1, -1, -1):
+        reverse.append(replaced[i])
+    reverse = ''.join(reverse)
+    return replaced == reverse
+
+def main():
+    print_result("A man, a plan, a canal: Panama", True, is_palindrome)
+    print_result("race a car", False, is_palindrome)
+    print_result(" ", True, is_palindrome)
+    print_result("ab_a", True, is_palindrome)
+
+if __name__ == '__main__':
+    main()
