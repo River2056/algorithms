bc1803f885e6a174128ae7352bdb0192722b68d2
river2056 <chen0625tung@gmail.com>
2022-04-14 01:21:31 +0800

    + modified leetcode:trap_rain_water problem(not finished)

--- problems/arrays/trap_rain_water.py
+++ problems/arrays/trap_rain_water.py
@@ -4,19 +4,28 @@ sys.path.insert(0, str(Path(os.path.abspath(__file__)).parent.parent.parent))
 from common import print_result
 
 def trap_rain_water(height):
-    walls = []
-    all_filled = []
+    all_filled = [*height]
+    stack = []
 
-    start_fill = False
     for index, block in enumerate(height):
-        if block > 0 or start_fill:
-            start_fill = True
-            
-        else:
-            all_filled.append(block)
+        if block > 0:
+            if len(stack) == 0:
+                stack.append((index, block))
+            elif len(stack) != 0:
+                peek_last_block = stack[-1]
+                if peek_last_block[1] < block and index - peek_last_block[0] > 1:
+
+            else:
+                last_block = stack.pop()
+                max_height = min(last_block[1], block)
+                if index - last_block[0] > 1:
+                    for i in range(last_block[0] + 1, index):
+                        all_filled[i] = max_height
+                stack.append((index, block))
 
     
-    print(walls)
+    print(height)
+    print(all_filled)
 
 def main():
     print_result([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6, trap_rain_water)
