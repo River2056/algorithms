36500a0cc5a9d4b91ca45cc0de603fce7fea65c7
Tung Chin Chen <chenp625tung@gmail.com>
2022-04-15 19:57:55 +0800

    trap_rain_water.py
    Trap_rain_water.py not finished

--- problems/arrays/trap_rain_water.py
+++ problems/arrays/trap_rain_water.py
@@ -3,31 +3,41 @@ from pathlib import Path
 sys.path.insert(0, str(Path(os.path.abspath(__file__)).parent.parent.parent))
 from common import print_result
 
-def trap_rain_water(height):
-    all_filled = [*height]
-    stack = []
-
+def trap_rain_water_brute_force(height):
+    # count every units of water for each element
+    total_amount = 0
     for index, block in enumerate(height):
-        if block > 0:
-            if len(stack) == 0:
-                stack.append((index, block))
-            elif len(stack) != 0:
-                peek_last_block = stack[-1]
-                if peek_last_block[1] < block and index - peek_last_block[0] > 1:
+        left_max = max(height[0:index]) if len(height[0:index]) > 0 else 0
+        right_max = max(height[index+1:]) if len(height[index+1:]) > 0 else 0
+        amount = min(left_max, right_max) - block
+        if amount <= 0:
+            continue
+        total_amount += amount
+    return total_amount
 
-            else:
-                last_block = stack.pop()
-                max_height = min(last_block[1], block)
-                if index - last_block[0] > 1:
-                    for i in range(last_block[0] + 1, index):
-                        all_filled[i] = max_height
-                stack.append((index, block))
+def trap_rain_water(height):
+    total_amount = 0
+    pl = 0
+    pr = len(height) - 1
+    max_left = 0
+    max_right = 0
+    while pl < pr:
+        current = height[pl]
+        max_left = max(height[pl], max_left)
+        max_right = max(max_right, height[pr])
+        amount = min(max_left, max_right) - current
+        total_amount += amount if amount >= 0 else 0
 
-    
-    print(height)
-    print(all_filled)
+        if height[pl] < height[pr]:
+            pl += 1
+        else:
+            pr -= 1
+    return total_amount
 
 def main():
+    print_result([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6, trap_rain_water_brute_force)
+    print_result([4, 2, 0, 3, 2, 5], 9, trap_rain_water_brute_force)
+
     print_result([0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1], 6, trap_rain_water)
     print_result([4, 2, 0, 3, 2, 5], 9, trap_rain_water)
 
