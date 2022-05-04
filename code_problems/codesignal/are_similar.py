826d1c62b5b6b57a3d05b623e8b7bc8f791dc6e8
Tung Chin Chen <chenp625tung@gmail.com>
2022-05-05 07:58:18 +0800

    finished are_similar.py
    + modified and finished are_similar.py

--- code_problems/codesignal/are_similar.py
+++ code_problems/codesignal/are_similar.py
@@ -15,12 +15,12 @@ def are_similar(a, b):
             return False
     check_if_content_match = sum([val if val > 0 else 0 for val in ref.values()]) == 0
     count = 0
-    if check_if_content_match == 0:
+    if check_if_content_match:
         # check position match
         for idx, _ in enumerate(a):
             if a[idx] != b[idx]:
                 count += 1
-    return check_if_content_match and count <= 1
+    return check_if_content_match and (count / 2) <= 1
 
 class TestAreSimilar(unittest.TestCase):
     def setUp(self):
@@ -28,7 +28,8 @@ class TestAreSimilar(unittest.TestCase):
             (([1, 2, 3], [1, 2, 3]), True),
             (([1, 2, 3], [2, 1, 3]), True),
             (([1, 2, 2], [2, 1, 1]), False),
-            (([832, 998, 148, 570, 533, 561, 894, 147, 455, 279], [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]), False)
+            (([832, 998, 148, 570, 533, 561, 894, 147, 455, 279], 
+              [832, 570, 148, 998, 533, 561, 455, 147, 894, 279]), False)
         ]
 
     def test_are_similar(self):
@@ -40,4 +41,3 @@ class TestAreSimilar(unittest.TestCase):
 
 if __name__ == '__main__':
     unittest.main()
-
