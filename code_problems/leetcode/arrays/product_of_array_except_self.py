f73ae4553c2e5cc9fea73060d822fad083869084
kevin.tung <chen0625tung@gmail.com>
2022-05-12 23:36:22 +0800

    + added comments for product_of_array_except_self

--- code_problems/leetcode/arrays/product_of_array_except_self.py
+++ code_problems/leetcode/arrays/product_of_array_except_self.py
@@ -3,6 +3,38 @@ import functools
 
 
 def product_of_array_except_self(nums: list[int]) -> list[int]:
+    """
+    go forwards for prefix, then backwards for postfix
+    [1, 2, 3, 4]
+
+    first iteration:
+    pre = 1
+    [1]
+    pre = pre * nums[0] == 1
+
+    [1, 1]
+    pre = pre * nums[1] == 2
+
+    [1, 1, 2]
+    pre = pre * nums[2] == 6
+
+    [1, 1, 2, 6]
+
+    second iteration(backwards):
+    post = 1
+
+    result[3] = post * result[3] == 1 * 6
+    post = post * nums[3] == 4
+
+    result[2] = post * result[2] == 4 * 2
+    post = post * nums[2] == 12
+
+    result[1] = post * result[1] == 12 * 1
+    post = post * nums[1] == 24
+
+    result[0] = post * ressult[0] == 24 * 1
+    post = post * nums[0] == 24
+    """
     result = [0] * len(nums)
     pre = 1
     for i in range(len(nums)):
