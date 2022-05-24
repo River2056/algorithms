9e8019e03d7898f6827b0f70ec2fb7f5557b537c
kevin.tung <chen0625tung@gmail.com>
2022-05-24 13:44:39 +0800

    + finished evaluate_reverse_polish_notation

--- code_problems/leetcode/arrays/evaluate_reverse_polish_notation.py
+++ code_problems/leetcode/arrays/evaluate_reverse_polish_notation.py
@@ -1,30 +1,51 @@
-def evalRPN(tokens: list[str]) -> int:
-    # operators = ["+", "-", "*", "/"]
+def evalRPN_self(tokens: list[str]) -> int:
     stack = []
-    previous = None
     for _, val in enumerate(tokens):
         try:
             number = int(val)
             stack.append(number)
         except ValueError:
+            previous = stack.pop()
             top = stack.pop()
-            if not previous:
-                previous = top
-                top = stack.pop()
-            previous = eval(f"{previous}{val if val != '/' else '//'}{top}")
-            print(f"{previous}{val if val != '/' else '//'}{top}")
+            res = int(eval(f"{top}{val}{previous}"))
+            stack.append(res)
 
-    return previous  # type: ignore
+    return stack.pop()  # type: ignore
+
+
+def evalRPN(tokens: list[str]) -> int:
+    operators = {
+        "+": lambda a, b: a + b,
+        "-": lambda a, b: a - b,
+        "*": lambda a, b: a * b,
+        "/": lambda a, b: int(a / b),
+    }
+    stack = []
+
+    for _, val in enumerate(tokens):
+        if val in operators:
+            num1 = int(stack.pop())
+            num2 = int(stack.pop())
+            res = operators[val](num2, num1)
+            stack.append(res)
+        else:
+            stack.append(val)
+
+    return stack.pop()
 
 
 def main():
-    # tokens = ["2", "1", "+", "3", "*"]
-    # result = evalRPN(tokens)
-    # print(result)
+    tokens = ["12", "7", "-"]
+    result = evalRPN(tokens)
+    print(result)
 
-    # tokens = ["4", "13", "5", "/", "+"]
-    # result = evalRPN(tokens)
-    # print(result)
+    tokens = ["2", "1", "+", "3", "*"]
+    result = evalRPN(tokens)
+    print(result)
+
+    tokens = ["4", "13", "5", "/", "+"]
+    result = evalRPN(tokens)
+    print(result)
 
     tokens = ["10", "6", "9", "3", "+", "-11", "*", "/", "*", "17", "+", "5", "+"]
     result = evalRPN(tokens)
